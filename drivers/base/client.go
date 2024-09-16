3845f6c94e7313c11f0647d571ca270473b01093
HenryZZZZZ <40875274+z-henry@users.noreply.github.com>
2024-09-17 00:55:40 +0800

    Update client.go

--- drivers/base/client.go
+++ drivers/base/client.go
@@ -14,7 +14,7 @@ var (
 	RestyClient      *resty.Client
 	HttpClient       *http.Client
 )
-var UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+var UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0"
 var DefaultTimeout = time.Second * 30
 
 func InitClient() {
